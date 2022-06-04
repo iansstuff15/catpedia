e3d07d6779c51cb38201445fa8e75ff75998b8e9
KrisD00 <61658901+KrisD00@users.noreply.github.com>
2022-06-04 20:44:23 +0800

    Update breedlist.dart

--- lib/screens/breedlist.dart
+++ lib/screens/breedlist.dart
@@ -10,75 +10,71 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
-class Breed{
+class Breed {
   Breed({
     required this.id,
     required this.name,
-
-
   });
   final String id;
   final String name;
 
   factory Breed.fromJson(Map<String, dynamic> json) => Breed(
-    id: json['id'],
-    name: json['name'],
-
-  );
-
+        id: json['id'],
+        name: json['name'],
+      );
 }
-class Breedlist extends StatefulWidget {
 
+class Breedlist extends StatefulWidget {
   @override
   _BreedlistState createState() => _BreedlistState();
 }
 
 class _BreedlistState extends State<Breedlist> {
-  final List<Breed> _breed=[];
-    Future<List<Breed>> fetchJson() async{
-      const url= 'https://api.thecatapi.com/v1/breeds';
-    const key='8f9719f3-c53c-4aa0-82f0-4107493d8c21';
+  final List<Breed> _breed = [];
+  Future<List<Breed>> fetchJson() async {
+    const url = 'https://api.thecatapi.com/v1/breeds';
+    const key = '8f9719f3-c53c-4aa0-82f0-4107493d8c21';
     Uri uri = Uri.parse(url);
     var response = await http.get(uri, headers: {"x-api-key": key});
-    List<Breed> blist=[];
+    List<Breed> blist = [];
 
-    if(response.statusCode==200){
-      log(response.body);
-      var urjson=json.decode(response.body);
-      for(var jsondata in urjson){
+    if (response.statusCode == 200) {
+      //log(response.body);
+      var urjson = json.decode(response.body);
+      for (var jsondata in urjson) {
         blist.add(Breed.fromJson(jsondata));
       }
     }
     return blist;
   }
+
   @override
-  void initState(){
-      fetchJson().then((value){
-        setState((){
-          _breed.addAll(value);
-        });
+  void initState() {
+    fetchJson().then((value) {
+      setState(() {
+        _breed.addAll(value);
       });
-      super.initState();
+    });
+    super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          
-        ),
-        body: ListView.builder(itemBuilder: (context, index){
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView.builder(
+        itemBuilder: (context, index) {
           return Card(
-          child: Column(
-          children: [
-        ],
-          ),
+            child: Column(
+              children: [
+                Text(_breed[index].id.toString()),
+                Text(_breed[index].name.toString())
+              ],
+            ),
           );
         },
-          itemCount: _breed.length,
-        ),
-      );
+        itemCount: _breed.length,
+      ),
+    );
   }
-
 }
-
