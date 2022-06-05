6bf9ba407bdffb9a62fc591d29f2881f4f2b86cd
Aryanna Manalastas <manalastasyanna@gmail.com>
2022-06-05 10:28:13 +0800

    added new assets, catInfo screen & added to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,25 @@
 import 'package:catpedia/screens/home.dart';
 import 'package:catpedia/screens/search.dart';
+import 'package:catpedia/screens/cat_info.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Catpedia',
-      
       initialRoute: Home.id,
-
       routes: {
-        Home.id: (context)=>Home(),
-        Search.id:(context) => Search(),
+        Home.id: (context) => Home(),
+        Search.id: (context) => Search(),
+        catInfo.id: (context) => catInfo(),
       },
     );
   }
 }
-
