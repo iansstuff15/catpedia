e2f450bc6d9060d50c2efc89261c716bd55dd99c
amaiYume <zehlljael@gmail.com>
2022-06-06 17:02:33 +0800

    update About us page

--- lib/screens/question.dart
+++ lib/screens/question.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:animations/animations.dart';
-
+import 'package:google_fonts/google_fonts.dart';
 
 class Question extends StatefulWidget {
   static String id = "question";
@@ -69,8 +69,97 @@ SafeArea _TutorialContent({required BuildContext context}) {
 
 SafeArea _Aboutus({required BuildContext context}) {
   return SafeArea(
-              child: GestureDetector(
-                  onTap: () => Navigator.pop(context),
-                  child: const Text('Hello')),
-            );
+        child: Scaffold(
+          backgroundColor: const Color.fromARGB(255, 165, 163, 163),
+          appBar: AppBar(
+
+            title: const Text("About us"),
+            backgroundColor: const Color.fromARGB(255, 151, 123, 10),
+          ),
+          body: Column(
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:   <Widget>[
+                      _CardCreator(name: "Jael, Zehll Fierran Day D.", image: 'profpic.jpg', email: 'qzfddjael@tip.edu.ph'),
+                      _CardCreator(name: "Bormate, Kristian Dereck D.", image: 'Kprofpic.jpg', email: 'qkddbormate@tip.edu.ph'),
+                      _CardCreator(name: "Manalastas, Aryanna B. ", image: 'YProf_pic.png', email: 'qagbmanalastas@tip.edu.ph'),
+                      _CardCreator(name: "Melegrito, John Adrian T.", image: 'Aprofpic.jpg', email: 'qjatmelegrito@tip.edu.ph'),
+                      _CardCreator(name: "Powell, Dirangen D.", image: 'Dprofpic.jpg', email: 'qdldppowell@tip.edu.ph'),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ),
+      );
+}
+
+Row _CardCreator({required String name, required String image, required String email}){
+  return 
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.white,
+                      backgroundImage: AssetImage('assets/$image'),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 10),
+                      child: Text('CS Student',
+                        style: GoogleFonts.macondo(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w800,
+                        ),
+                      ),
+                    )
+                             ],
+                      ),
+                 Column(children: [
+                     Text(name,
+                      style: GoogleFonts.trirong(
+                      fontSize: 17,
+                      fontWeight: FontWeight.w900,                   
+                      )
+                     ),
+                    const SizedBox(
+                       width: 250,
+                       height: 60,      
+                       child: Card(
+                         child: ListTile(
+                           contentPadding: EdgeInsets.symmetric(horizontal:10),
+                           leading: Icon(Icons.phone),
+                           title: Text("09121124323",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500
+                            ),
+                           )
+                         )
+                       ),
+                     ),
+                    SizedBox(
+                       width: 250,
+                       height: 60,
+                       child: Card(
+                         child: ListTile(
+                           contentPadding: const EdgeInsets.symmetric(horizontal:10),
+                           leading: const Icon(Icons.email),
+                           title: Text(email,
+                           style: const TextStyle(fontSize: 12, fontWeight: FontWeight.w500)
+                           ),
+                         )
+                       ),
+                     )
+                    ]          
+                    ),
+
+
+              ],);
 }
