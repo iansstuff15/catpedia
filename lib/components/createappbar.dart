e61530fed781085fbee0b336840126e1d77827ba
amaiYume <zehlljael@gmail.com>
2022-06-06 19:05:27 +0800

    update appbar colors

--- lib/components/createappbar.dart
+++ lib/components/createappbar.dart
@@ -10,19 +10,22 @@ import 'package:flutter/material.dart';
 // }
 Container createAppBar({required BuildContext context, required String title}){
   return Container(
-                height: 30,
+                height: 50,
                 color: const Color.fromARGB(255, 151, 123, 10),
                 child: Row(
                   
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                       IconButton(
-                        icon: const Icon(Icons.arrow_back),
+                        icon: const Icon(Icons.arrow_back,
+                        color: Colors.white,
+                        ),
                         onPressed: () => Navigator.pop(context),
                         ),
                       Text(title,
                         style: const TextStyle(
                           fontSize: 24,
+                          color: Colors.white
                         )
                       ),
                       const SizedBox(),
